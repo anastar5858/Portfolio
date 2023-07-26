9cf11cd121559583134b16aee353701418ecf8d5
Anas Tarrab <71597147+UP920130@users.noreply.github.com>
2023-07-26 05:15:50 +0100

    Images and src keys

--- data/additionalData.js
+++ data/additionalData.js
@@ -2,26 +2,37 @@ const Books = [{
     'Turtles All the Way Down': [{
         attribute: 'Compassion',
         aspect: 'The compassion of an OCD patient with a normal man',
+        src: './images/AddImg/additionalContentImages/Books/turtles.jpg',
     }, 
     {
         attribute: 'No Judge',
         aspect: 'How the normal guy never judged the action of his OCD lover',
+        src: './images/AddImg/additionalContentImages/Books/turtles.jpg',
+
     }, {
         attribute: 'Compensation',
         aspect: 'Bi-directional compensation between the two psychologically different beings',
+        src: './images/AddImg/additionalContentImages/Books/turtles.jpg',
+
     }
 ]
 }, {
     'Sherlock Holmes': [{
         attribute: 'Intuition',
         aspect: 'Analysing scenes by only intuitive skills',
+        src: './images/AddImg/additionalContentImages/Books/Sherlock.jpg',
+
     }, 
     {
         attribute: 'Detective skills',
         aspect: 'Throughout the variety of cases presented',
+        src: './images/AddImg/additionalContentImages/Books/Sherlock.jpg',
+
     }, {
         attribute: 'Eye for peculiar things',
         aspect: 'Examining what others would typically ignore',
+        src: './images/AddImg/additionalContentImages/Books/Sherlock.jpg',
+
     }
 ]
 
@@ -29,13 +40,19 @@ const Books = [{
     'Death\'s End': [{
         attribute: 'Cognitive skills',
         aspect: 'The complexity of the introduced theories',
+        src: './images/AddImg/additionalContentImages/Books/Death\'s.jpg',
+
     }, 
     {
         attribute: 'Science',
         aspect: 'Helped with a lot of concepts such as propulsion and Centrifugal force',
+        src: './images/AddImg/additionalContentImages/Books/Death\'s.jpg',
+
     }, {
         attribute: 'Problem-solving',
         aspect: 'How physics problems where tackled in many different scientific approaches',
+        src: './images/AddImg/additionalContentImages/Books/Death\'s.jpg',
+
     }
 ]
 },
@@ -43,13 +60,19 @@ const Books = [{
     'The Lord Of the Rings': [{
         attribute: 'Strategical thinking',
         aspect: 'Through the planning of many battles',
+        src: './images/AddImg/additionalContentImages/Books/LOTR.jpg',
+
     }, 
     {
         attribute: 'Imagination',
         aspect: 'The author wide imagination of various realms',
+        src: './images/AddImg/additionalContentImages/Books/LOTR.jpg',
+
     }, {
         attribute: 'Choices',
         aspect: 'How characteres influenced the world with their choices',
+        src: './images/AddImg/additionalContentImages/Books/LOTR.jpg',
+
     }
 ]
 
@@ -58,13 +81,19 @@ const Books = [{
     'Rich Dad, Poor Dad': [{
         attribute: 'Investement mindset',
         aspect: 'The many advocates for investment',
+        src: './images/AddImg/additionalContentImages/Books/Rich.jpg',
+
     }, 
     {
         attribute: 'Money circulation',
         aspect: 'The emphasis on keeping the money moving',
+        src: './images/AddImg/additionalContentImages/Books/Rich.jpg',
+
     }, {
         attribute: 'Savings',
         aspect: 'Stop wasting on many unnecessary spendings',
+        src: './images/AddImg/additionalContentImages/Books/Rich.jpg',
+
     }
 ]
 }
@@ -77,26 +106,38 @@ const Movies = [
         'Horror': [{
             movie: 'The Exorcism',
             rationale: 'One of the best vintage films, showing the devil\'s work on humans.',
+            src: './images/AddImg/additionalContentImages/Movies/Exorcism.jpg',
+
         }, 
         {
             movie: 'Resident Evil',
             rationale: 'Re-shaped zombie movies in a bigger way, specially the suspence with Alice\'s abilities and the cloning concept.',
+            src: './images/AddImg/additionalContentImages/Movies/RE.jpg',
+
         }, {
             movie: 'The Conjuring',
             rationale: 'Taking the best of the classical masterpieces and resurrecting the contemporary version of the genre.',
+            src: './images/AddImg/additionalContentImages/Movies/Con.jpg',
+
         }
     ]
     }, {
         'Thriller': [{
             movie: 'The Dark Knight',
             rationale: 'The exquisite acting of the Joker, and the translation of his sufferings to a non-distinguishable smile.',
+            src: './images/AddImg/additionalContentImages/Movies/Dark.jpg',
+
         }, 
         {
             movie: 'Se7en',
             rationale: 'The clever detective work & criminal cunning driving each other.',
+            src: './images/AddImg/additionalContentImages/Movies/Se7en.jpg',
+
         }, {
             movie: 'Green Mile',
             rationale: 'Showcasing the message of how humanity can destriy its own miracles.',
+            src: 'images/AddImg/additionalContentImages/Movies/Green.jpg',
+
         }
     ]
     
@@ -104,13 +145,18 @@ const Movies = [
         'Action': [{
             movie: 'Inception',
             rationale: 'The theory of dreams inside dreams & the recursive influence on each other is mesmerising.',
+            src: './images/AddImg/additionalContentImages/Movies/Inception.jpg',
+
         }, 
         {
             movie: 'John Wick',
             rationale: 'Action-packed movie that never stops for a second.',
+            src: './images/AddImg/additionalContentImages/Movies/John.jpg',
+
         }, {
             movie: 'Ocean Eleven',
             rationale: 'A series of heists with intelligent & diligent planning.',
+            src: './images/AddImg/additionalContentImages/Movies/Ocean.jpg',
         }
     ]
     },
@@ -118,13 +164,16 @@ const Movies = [
         'Science Fiction': [{
             movie: 'Guardians of the Galaxy Vol.3',
             rationale: 'Visualising the greedy nature of humans towards pursuing perfection & stacked with emotional triggers.',
+            src: './images/AddImg/additionalContentImages/Movies/Guardians.jpg',
         }, 
         {
             movie: 'Avatar',
             rationale: 'The ability to transfer conscious and memories to extra-terrestrial bodies is simply astonishing.',
+            src: './images/AddImg/additionalContentImages/Movies/Avatar.jpg',
         }, {
             movie: 'Interstellar',
             rationale: 'The best movie showcasing astronomical theories',
+            src: './images/AddImg/additionalContentImages/Movies/Inter.jpg',
         }
     ]
     
@@ -133,13 +182,16 @@ const Movies = [
         'Comedy': [{
             movie: 'Home Alone',
             rationale: 'The Christmas Eve classical speaks for itself.',
+            src: './images/AddImg/additionalContentImages/Movies/Home.jpg',
         }, 
         {
             movie: 'Coming To America',
             rationale: 'A clever production of a humble king in civil disguise',
+            src: './images/AddImg/additionalContentImages/Movies/America.jpg',
         }, {
             movie: 'MASK',
             rationale: 'Adopted from one of my favourite animation series.',
+            src: './images/AddImg/additionalContentImages/Movies/MASK.jpg',
         }
     ]
     }
@@ -153,26 +205,38 @@ const Hobbies = [
         'Weight liting': [{
             exercise: 'Bench Press',
             pr: '75Kg',
+            src: './images/AddImg/additionalContentImages/Hobbies/Bench.jpg',
+
         }, 
         {
             exercise: 'Deadlift',
             pr: '110Kg',
+            src: './images/AddImg/additionalContentImages/Hobbies/Dead.jpg',
+
         }, {
             exercise: 'Squat',
             pr: '75Kg',
+            src: './images/AddImg/additionalContentImages/Hobbies/Squat.jpg',
+
         }
     ]
     }, {
         'Learning new languages': [{
             Language: 'Chinese',
             rationale: 'A fascinating symbolic language spoken by a billion people. The challenge of learning it is the main driver.',
+            src: './images/AddImg/additionalContentImages/Hobbies/China.jpeg',
+
         }, 
         {
             Language: 'English',
             rationale: 'A must for today\'s life & always interesting to learn and upscale its vocubulary.',
+            src: './images/AddImg/additionalContentImages/Hobbies/English.jpg',
+
         }, {
             Language: 'Arabic',
             rationale: 'The native language with a long wa to reach a tangent of perfection, as the vocabulary shocks even us.',
+            src: './images/AddImg/additionalContentImages/Hobbies/Arabic.png',
+
         }
     ]
     
@@ -180,24 +244,34 @@ const Hobbies = [
         'Multi-disciplinary books reading': [{
             Book: 'GA & ML for programmers',
             Discipline: 'A realm of natural biological phenomena impact on computing.',
+            src: './images/AddImg/additionalContentImages/Hobbies/GAML.jpg',
+
         }, 
         {
             Book: 'Pattern Recognision & ML',
             Discipline: 'A pure mathematical approach in tackling Machine Learning. (In progress).',
+            src: './images/AddImg/additionalContentImages/Hobbies/PRML.jpg',
+
         }
     ]
     },
     {
         'Gaming': [{
-            game: 'Guardians of the Galaxy Vol.3',
-            rationale: 'Visualising the greedy nature of humans towards pursuing perfection & stacked with emotional triggers.',
+            game: 'Resident Evil 4',
+            rationale: 'The introduction of third person view to graphics, the story, the mixture of horro and action, is all on-spot.',
+            src: './images/AddImg/additionalContentImages/Hobbies/RE4.webp',
+
         }, 
         {
-            game: 'Avatar',
-            rationale: 'The ability to transfer conscious and memories to extra-terrestrial bodies is simply astonishing.',
+            game: 'Devil May Cry',
+            rationale: 'The intellectual journey to the imaginary depth of the underworld.',
+            src: './images/AddImg/additionalContentImages/Hobbies/DMC.jpg',
+
         }, {
-            game: 'Interstellar',
-            rationale: 'The best movie showcasing astronomical theories',
+            game: 'Horizon Forbidden West',
+            rationale: 'Showcasing the potential future of humanity after an AI terraforming system goes rogue.',
+            src: './images/AddImg/additionalContentImages/Hobbies/HRFW.webp',
+
         }
     ]
     
@@ -206,13 +280,16 @@ const Hobbies = [
         'Travelling solo/social circle': [{
             destination: 'Austria',
             rationale: 'The first portal to international travel that inspired me to explore more, and lose weight for future hard hikes.',
+            src: './images/AddImg/additionalContentImages/Hobbies/Aus.webp',
         }, 
         {
             destination: 'UK',
             rationale: 'Where I established my Bachelor degree, along with the mixture of memories of my adolescence.',
+            src: './images/AddImg/additionalContentImages/Hobbies/UK.jpg',
         }, {
             destination: 'Hong Kong (China)',
             rationale: 'A different taste away from the typical western cultures, was simply extraordinary with tech & architecture evolutions.',
+            src: './images/AddImg/additionalContentImages/Hobbies/HK.jpg',
         }
     ]
     }
