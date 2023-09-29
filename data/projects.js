46c04218ffa969865b1aec5ff775f56bebf99ea0
anastar5858 <anas.tarrab58@gmail.com>
2023-09-29 11:25:47 +0100

    Projects data ready

---
+++ data/projects.js
@@ -0,0 +1,76 @@
+const projects = [
+    {
+        name: 'Furry Friends',
+        img: './images/Projects/Furry Friends.jpg',
+        desc: `The first project during my training with the company Kreativstorm. The essence
+        was gaining industry skills by working in a team to adhere to client specs, in order
+        to develop the website design as requested.`,
+        skills: ['Team Work', 'CSS',  'HTML Semantics', 'Documentation Analysis', 'Communication'],
+        link: 'https://anastar5858.github.io/main_css/',
+    },
+    {
+        name: 'Rock, Paper, Scissors',
+        img: './images/Projects/Rock Papers Scissors.png',
+        desc: `The second project during my training with the company Kreativstorm. The challenge behind
+        this section of the training was learning JavaScript from a different perspective, to build 
+        a functionality considering Front-End practices without any other language except the HTML boilerplate.
+        In this project Levenshtein Distance was implemented for user input correction as an extra treat to be
+        familiarised with the algorithm.`,
+        skills: ['Usability', 'Team Work', 'Communication', 'JavaScript', 'User Experience', 'Auto Correction'],
+        link: 'https://anastar5858.github.io/main_js.1/',
+    },
+    {
+        name: 'Animation Calculator',
+        img: './images/Projects/Calculator.jpg',
+        desc: `The third project during my training with the company Kreativstorm. The last team project held, 
+        combining the web development triplets (CSS, Javascript & HTML), in order to build a simple calculator.
+        Animations has been introduced to make this calculator fun for children usage, as well as keyboard 
+        functionality and a dark mode. The project deviated from conventional design to broaden creativity.`,
+        skills: ['Team Work', 'CSS', 'HTML', 'JavaScript', '2d  Web Animation', 'UI design', 'Creativity'],
+        link: 'https://anastar5858.github.io/main_js.2/',
+    },
+    {
+        name: 'Services For People',
+        img: './images/Projects/Services for people.jpg',
+        desc: `The first hand on experience in a full-stack development team environment during university.
+        The task consisted of building a platform for linking charities/donors with people in need 
+        (Medicine, Food etc). I had my first interraction with the concept of API's, unit, integration, and 
+        mainly end to end testing as well as local (on-disk) SQL databases throughout this assignment`,
+        skills: ['Software Testing', `API's`, 'Local SQL Database'],
+        link: 'https://servicesforpeople-3124ef39287d.herokuapp.com/',
+    },
+    {
+        name: 'Small & Medium Sized Enterprises Hub',
+        img: './images/Projects/SME.jpg',
+        desc: `A project handed by a client during university, which introduced the idea of building a dedicated platform
+        for linking healthcare companies with opportunities providers based on their various needs 
+        (could be funds, equipments, staff shortage etc.). In this attemp a natrual language project
+        was enacted using primitive models, with the introduction of a matching algorithms. The preliminary
+        results were commended by the clients and put to further development from another team.`,
+        skills: ['Natural Language Processing', 'Software Methodology', 'Software Architecture', 'Node.js', 'MongoDB Atlas', 'Problem Solving', 'Algorithm Design'],
+        link: 'https://sme-0dd9d080084b.herokuapp.com/',
+    },
+    {
+        name: 'Website Demonstarion Tool',
+        img: './images/Projects/Web Demo.jpg',
+        desc: `A simple university project to demonstrate the idea of a tool applicable to all websites with 
+        a focus on React.js applications, that provides developers to construct interractive 
+        demonstrations of their features by utilizing DOM events, without 
+        the need of additional configurations mandated by similar projects.`,
+        skills: ['React.js', 'Software Bundlers', 'Back End Practices', 'In-depth DOM Manipulation'],
+        link: 'https://csv-handler.herokuapp.com/',
+    },
+    {
+        name: 'Universities Administration Bot',
+        img: './images/Projects/Admin Bot.jpg',
+        desc: `My final year project, which was building a Discord Bot that is able to automatically
+        register students, forward them to course related channels, by analysing the university 
+        students records without the need of human administrations. Web crawling was used to extract
+        course information from the university designated course website, integrating the bot with the website
+        using an API to validate course data to organise the server accordingly, the student data in CSV files
+        were used to validate and register students within the Discord server. Automation was incorporated more
+        than 50pc.`,
+        skills: ['Full Stack Development', 'Bot Development', 'MongoDB', 'Web Crawling', 'Complete Software Testing'],
+        link: 'https://universitycsv-88e3a553aad6.herokuapp.com/',
+    },
+]
